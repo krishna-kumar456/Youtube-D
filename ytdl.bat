0897b95476672afb0c17311b345bcd92d632e768
redfruitt <krishnaqmar@gmail.com>
2016-02-29 00:10:17 +0530

    Update ytdl.bat

--- ytdl.bat
+++ ytdl.bat
@@ -1,4 +1,4 @@
 @echo off
-cd C:/Users/Krishna/Downloads/Music
+cd C:/path/
 set /p id="Enter id:"
 youtube-dl --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest https://www.youtube.com/watch?v=%id%
